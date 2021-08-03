263471e51f39b220090dba54c03a3e6584a44ade
tc-miyatani <miyahei38+tc@gmail.com>
2021-08-03 17:56:23 +0900

    user関連ヘルパーメソッド実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def current_user
+    if user_auth_mail_signed_in?
+      current_user_auth_mail.user
+    elsif user_auth_provider_signed_in?
+      current_user_auth_provider.user
+    else
+      nil
+    end
+  end
+
+  def user_signed_in?
+    user_auth_mail_signed_in? || user_auth_provider_signed_in?
+  end
 end
