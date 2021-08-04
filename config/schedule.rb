194af2da79f783e8de68facdc6a25db458c0546f
tc-miyatani <miyahei38+tc@gmail.com>
2021-08-04 14:47:37 +0900

    cronの実行時にrbenvの初期化をするように修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,9 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '/environment')
 rails_env = ENV['RAILS_ENV'] || :development
 set :environment, rails_env
 set :output, "#{Rails.root}/log/cron.log"
+job_type :rbenv_rake, 'eval "$(rbenv init -)" && cd :path && :environment_variable=:environment bundle exec rake :task --silent :output'
 
 every 1.minutes do
-  rake 'task_reminder:for_cron_test',
+  rbenv_rake 'task_reminder:for_cron_test',
     environment_variable: 'RAILS_ENV',
     environment: rails_env
 end
--- lib/tasks/task_reminder.rake
+++ lib/tasks/task_reminder.rake
@@ -1,11 +1,15 @@
 namespace :task_reminder do
   desc '通知実行'
   task :notification do
-    p 'OK!'
+    p Time.current.strftime('%Y-%m-%d %H:%M') + ' NOTIFICATION START!'
+    # 通知するものがあるか探す
+    # 通知を実行する
+    # 通知テーブルのレコード更新・削除
+    # 通知ログテーブルにログ追加
   end
 
   desc  '自動実行テスト'
   task :for_cron_test do
-    p 'Hello'
+    p Time.current.strftime('%Y-%m-%d %H:%M') + ' OK!'
   end
 end
