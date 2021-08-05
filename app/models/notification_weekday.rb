ccd723112136e49e6410d271b345d57bf0ada49e
tc-miyatani <miyahei38+tc@gmail.com>
2021-08-05 23:47:37 +0900

    notification_weekdayバリデーション設定

--- app/models/notification_weekday.rb
+++ app/models/notification_weekday.rb
@@ -1,2 +1,12 @@
 class NotificationWeekday < ApplicationRecord
+  belongs_to :reminder
+
+  with_options presence: true do
+    validates :weekday_id, numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 0,
+      less_than_or_equal_to: 6
+    }
+    validates :reminder
+  end
 end
