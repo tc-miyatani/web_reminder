3670bd14d1268f1a9fd632c69cb38dac3f0e0b50
tc-miyatani <miyahei38+tc@gmail.com>
2021-08-14 14:24:57 +0900

    asset関係のヘルパーメソッドとreactのmodule作成

--- app/helpers/react_pages_helper.rb
+++ app/helpers/react_pages_helper.rb
@@ -1,2 +1,14 @@
 module ReactPagesHelper
+  EXTENSIONS = ['jpg', 'png']
+
+  def assets_all
+    assets_hash = {}
+    EXTENSIONS.each do |ext|
+      Dir.glob(Rails.root.join('app', 'assets', 'images', "*.#{ext}").to_s) do |filepath|
+        filename = File.basename(filepath)
+        assets_hash[filename] = asset_path(filename)
+      end
+    end
+    assets_hash
+  end
 end
