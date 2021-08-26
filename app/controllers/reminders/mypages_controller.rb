14056b9431c2f54d5ee177060d2c3659726e1e4f
tc-miyatani <miyahei38+tc@gmail.com>
2021-08-26 16:14:44 +0900

    loadingを見せる為のsleep

--- app/controllers/reminders/mypages_controller.rb
+++ app/controllers/reminders/mypages_controller.rb
@@ -10,6 +10,7 @@ class Reminders::MypagesController < ApplicationController
   end
 
   def update
+    sleep 2.0 # loadingを見せるためにわざと遅らせる
     if current_user.update(params.require(:user).permit(:nickname))
       render json: {is_success: true, msg: '更新が完了しました！'}
     else
