830e239c8be88e01a8450a94ea736457a116b44f
tc-miyatani <miyahei38+tc@gmail.com>
2021-09-03 01:07:47 +0900

    自動生成されたファイル

---
+++ spec/mailers/add_mail_mailer_spec.rb
@@ -0,0 +1,5 @@
+require "rails_helper"
+
+RSpec.describe AddMailMailer, type: :mailer do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
---
+++ spec/mailers/previews/add_mail_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/add_mail_mailer
+class AddMailMailerPreview < ActionMailer::Preview
+
+end
