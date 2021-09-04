ac0f0485ee30111db5fb3f322559eb76602b9a9b
tc-miyatani <miyahei38+tc@gmail.com>
2021-09-05 02:25:13 +0900

    user_providerの初期値設定処理

--- app/models/user_auth_provider.rb
+++ app/models/user_auth_provider.rb
@@ -5,14 +5,21 @@ class UserAuthProvider < ApplicationRecord
   belongs_to :user
 
   with_options presence: true do
-    validates :provider_id
+    validates :provider_id, uniqueness: { scope: :provider_name }
     validates :provider_name
   end
 
   def self.create_with_user(user_auth_provider_params)
-    user_auth_provider = self.new(user_auth_provider_params)
-    user = user_auth_provider.create_user(auth_type: User::AUTH_TYPE_PROVIDER)
-    user_auth_provider.save
-    user_auth_provider
+    ActiveRecord::Base.transaction(joinable: false, requires_new: true) do
+      user_auth_provider = self.new(user_auth_provider_params)
+      user = user_auth_provider.create_user(auth_type: User::AUTH_TYPE_PROVIDER)
+      user_auth_provider.save
+      user.user_providers.create(
+        provider_id: user_auth_provider.provider_id,
+        provider_name: user_auth_provider.provider_name
+      )
+
+      user_auth_provider
+    end
   end
 end
--- lib/tasks/task_update.rake
+++ lib/tasks/task_update.rake
@@ -7,7 +7,7 @@ namespace :task_update do
     end
   end
 
-  desc '通知先追加に対応'
+  desc '通知先追加に対応(メール)'
   task :user_mails_default => :environment do
     User.where(auth_type: User::AUTH_TYPE_MAIL).each do |user|
       user_auth_mail = user.user_auth_mail
@@ -18,6 +18,13 @@ namespace :task_update do
         email: user_auth_mail.email,
         confirmed_at: user_auth_mail.confirmed_at
       )
+    end
+  end
+
+  desc '通知先追加に対応(LINE)'
+  task :user_providers_default => :environment do
+    User.includes(:user_auth_provider).where(auth_type: User::AUTH_TYPE_PROVIDER).find_each do |user|
+      UserProvider.find_or_create_by(user.user_auth_provider.attributes)
     end
   end
 end
