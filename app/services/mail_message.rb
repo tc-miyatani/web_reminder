1dd278f18ccb39f30c9f4b8de83a9c82dafc7110
tc-miyatani <miyahei38+tc@gmail.com>
2021-09-17 23:30:26 +0900

    複数の通知先へ通知実行

--- app/services/mail_message.rb
+++ app/services/mail_message.rb
@@ -1,6 +1,6 @@
 class MailMessage
-  def self.send_reminder(reminder)
+  def self.send_reminder(reminder_user_mail)
     # ReminderMailer.send_reminder(reminder).deliver_later
-    ReminderMailer.send_reminder(reminder).deliver_now
+    ReminderMailer.send_reminder(reminder_user_mail).deliver_now
   end
 end
--- app/services/reminder_notification.rb
+++ app/services/reminder_notification.rb
@@ -1,19 +1,14 @@
 class ReminderNotification
-  DEFAULT_SEND_REMINDERS = {
-    mail_message: MailMessage,
-    line_message: LineMessage
-  }
-
-  def self.send_reminder(reminder, send_reminders={})
-    if reminder.user_auth_type == 'user_auth_mail'
-      reminder_type = :mail_message
-    elsif reminder.user_auth_model.provider_name == 'line'
-      reminder_type = :line_message
-    else
-      raise RuntimeError.new('プロバイダー名に異常があります')
+  def self.send_reminder(reminder)
+    remider.reminder_user_mails.each do |reminder_user_mail|
+      MailMessage.send_reminder(reminder_user_mail)
     end
-
-    reminder_class = send_reminders[reminder_type] || DEFAULT_SEND_REMINDERS[reminder_type]
-    reminder_class.send_reminder(reminder)
+    reminder.reminder_user_providers.each do |reminder_user_provider|
+      if user_provider.provider_name == 'line'
+        LineMessage.send_reminder(reminder_user_provider)
+      else
+        raise RuntimeError.new('プロバイダー名に異常があります')
+      end
+    end
   end
 end
