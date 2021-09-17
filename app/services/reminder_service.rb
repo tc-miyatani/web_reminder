6c30ab4d2a15f07ba832164c62a7f90fdce7d4e9
tc-miyatani <miyahei38+tc@gmail.com>
2021-09-17 23:59:01 +0900

    次回通知時の算出

--- app/services/reminder_service.rb
+++ app/services/reminder_service.rb
@@ -32,24 +32,17 @@ class ReminderService
 
   # 次の通知日時を算出して返す
   def self.calc_next_time(reminder, base_time=Time.current)
-    if reminder.notification_time.blank?
-      reminder.notification_time = reminder.notification_datetime.strftime('%H:%M')
-    end
+    notification_time = reminder.notification_datetime.strftime('%H:%M')
 
     case reminder.repeat_type
-    when RepeatType.find_by(name: 'once') then
-      if reminder.notification_date.blank?
-        reminder.notification_date = reminder.notification_datetime.strftime('%Y-%m-%d')
-      end
-      next_time = reminder.notification_date + ' ' + reminder.notification_time
     when RepeatType.find_by(name: 'repeat-daily') then
-      next_time = self.daily_next_time(reminder.notification_time)
+      next_time = self.daily_next_time(notification_time)
     when RepeatType.find_by(name: 'repeat-weekly') then
       if reminder.weekdays.blank?
         next_time = nil
       else
         next_time = self.weekly_next_time(
-                                  reminder.notification_time,
+                                  notification_time,
                                   reminder.weekdays
                                 )   
       end
@@ -70,14 +63,14 @@ class ReminderService
   end
 
   # 指定曜日・指定時刻の直近の日時
-  def self.weekly_next_time(time_str, weekdays_str, now=Time.current)
+  def self.weekly_next_time(time_str, weekdays, now=Time.current)
     # 指定時刻を過ぎていなければ昨日を基準日に、過ぎていれば今日を基準日にする
     base_day = time_of_day(time_str, now) > now ? now.yesterday : now
     # 複数の選択している曜日の直近の指定日時を計算し(map)、その中から現在日時以降の(filter)、直近の日時(min)を取得
-    next_time = weekdays_str.map{ |wday|
+    next_time = weekdays.map{ |weekday|
       time_of_day(
         time_str,
-        base_day.next_occurring(WDAYS[wday.to_i]) # 直近の指定曜日(基準日と同じ曜日なら1週間後)
+        base_day.next_occurring(WDAYS[weekday.id]) # 直近の指定曜日(基準日と同じ曜日なら1週間後)
       )
     }.filter{|dt| dt > now}.min
     next_time.strftime('%Y-%m-%d %H:%M')
