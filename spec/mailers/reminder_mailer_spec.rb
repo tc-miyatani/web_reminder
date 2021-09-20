61ae5b80e82affe46d609395df76f894d20e22fd
tc-miyatani <miyahei38+tc@gmail.com>
2021-09-20 22:40:24 +0900

    テストコードの修正(リマインダーメール送信)

--- spec/mailers/reminder_mailer_spec.rb
+++ spec/mailers/reminder_mailer_spec.rb
@@ -3,16 +3,17 @@ require "rails_helper"
 RSpec.describe ReminderMailer, type: :mailer do
   describe '#send_reminer' do
     before(:all) do
-      @reminder = FactoryBot.build(:reminder)
-      @user_auth_mail = @reminder.build_user(id: 99999).build_user_auth_mail(email: ENV['GMAIL_FROM'])
-      described_class.send_reminder(@reminder).deliver_now
+      @reminder = FactoryBot.create(:reminder)
+      @user_mail = @reminder.create_user.user_mails.create(email: 'sample@com')
+      @reminder_user_mail = @reminder.reminder_user_mails.create(user_mail_id: @user_mail.id)
+      described_class.send_reminder(@reminder_user_mail).deliver_now
       @mail = ActionMailer::Base.deliveries.last
     end
 
     context 'リマインダーメール送信' do
       it 'from/to/subject/body' do
         expect(@mail.from.first).to eq ENV['MAIL_FROM']
-        expect(@mail.to.first).to   eq @user_auth_mail.email
+        expect(@mail.to.first).to   eq @user_mail.email
         expect(@mail.subject).to    eq 'CloudReminderによる通知'
         expect(@mail.body).to  include @reminder.message
       end
